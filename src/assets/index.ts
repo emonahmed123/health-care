cffe99aae4d432d7e1aef5a9f6f82ed3b04f3e5d
Emon Ahmed <imonshomon@gmail.com>
2025-03-06 22:21:14 +0600

    hero-section done

---
+++ src/assets/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const assets = {
+  images: {
+    doctor1: require("./images/doctor1.png"),
+    doctor2: require("./images/doctor2.png"),
+    doctor3: require("./images/doctor3.png"),
+    stethoscope: require("./images/Stetoscope.png"),
+  },
+};
+export default assets;
--- src/components/ui/HomePage/HeroSeciton.tsx
+++ src/components/ui/HomePage/HeroSeciton.tsx
@@ -1,5 +1,7 @@
+
 import { Box, Button, Container, Typography } from '@mui/material';
 import Image from 'next/image';
+import assets from '@/assets';
 import React from 'react';
 
 const HeroSeciton = () => {
@@ -50,7 +52,66 @@ const HeroSeciton = () => {
                     <Button variant="outlined">Contact us</Button>
                 </Box>
             </Box>
-            <Box>right</Box>
+            <Box sx={{
+                flex: 1, position: "relative", mt: 0,
+                justifyContent: "center",
+                display: "flex",
+                p: 1,
+                m: 1,
+            }}>
+                <Box
+                    sx={{
+                        display: "flex",
+                        gap: 2,
+                    }}
+                >
+                    <Box mt={4}>
+                        <Image
+                            src={assets.images.doctor1}
+                            width={240}
+                            height={380}
+                            alt="doctor1"
+                        />
+                    </Box>
+                    <Box>
+                        <Image
+                            src={assets.images.doctor2}
+                            width={240}
+                            height={350}
+                            alt="doctor2"
+                        />
+                    </Box>
+                </Box>
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "220px",
+                        left: "150px",
+                    }}
+                >
+                    <Image
+                        src={assets.images.doctor3}
+                        width={240}
+                        height={240}
+                        alt="doctor3"
+                    />
+                </Box>
+                <Box
+                    sx={{
+                        position: "absolute",
+                        bottom: "-50px",
+                        right: 0,
+                        zIndex: "-1",
+                    }}
+                >
+                    <Image
+                        src={assets.images.stethoscope}
+                        width={180}
+                        height={180}
+                        alt="doctor3"
+                    />
+                </Box>
+            </Box>
 
         </Container>
     );
